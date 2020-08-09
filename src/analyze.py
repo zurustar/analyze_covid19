8767bf73cd87dbe72f15edef9fa875b211204947
zurustar <zurustar@users.noreply.github.com>
2020-08-10 08:28:06 +0900

    updated

--- src/analyze.py
+++ src/analyze.py
@@ -3,16 +3,22 @@
 import pandas as pd
 import glob
 import sqlite3
+import sys
+import os
 
 def main(d):
+	output_filename = d + os.sep + "result.csv"
 	db = sqlite3.connect(':memory:')
-	for filename in glob.glob(d):
-		df = pd.read_csv(filename, dtype=str)
-		df['date'] = pd.to_datetime(df['公表_年月日'])
-		df.to_sql('covid19', db, if_exists='append', index=None)
+	for filename in glob.glob(d + os.sep + "*.csv"):
+		if filename == output_filename:
+			continue
+		print("loading", filename)
+		tmp = pd.read_csv(filename, dtype=str)
+		tmp['date'] = pd.to_datetime(tmp['公表_年月日'])
+		tmp.to_sql('covid19', db, if_exists='append', index=None)
 	df = pd.read_sql_query('SELECT date, count(date) FROM covid19 group by date;', db)
+	df.to_csv(output_filename)
 	db.close()
-	df.to_csv("./tmp.csv", header=None, index=None)
 
 if __name__ == '__main__':
-	main('./*.csv')
+	main(sys.argv[1])
