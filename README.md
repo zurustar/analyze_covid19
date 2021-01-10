dc9594dede24bd27d1614a71e8cae39db96cd78f
zurustar <zurustar@users.noreply.github.com>
2021-01-10 23:56:49 +0900

    updated

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 東京都が公開している新型コロナウイルス感染者数を記したcsvファイルをちょこっと加工してexcelで感染者数の推移を示すグラフを作成しやすくするだけのツール。
 
+## 使い方
+
 srcディレクトリ内に分析対象のcsvファイルを置いてから、
 docker-compose up
 を実行するとsrcディレクトリ内に出力されるresult.csvというファイルをExcelで読み込んでグラフを描くなどすることを想定。
+
+今日時点では以下のURLに対象となるファイルがおいてあった。
+https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_patients.csv
+
--- src/analyze.py
+++ src/analyze.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import matplotlib.pyplot as plt
 import glob
-import sqlite3
 import sys
 import os
-from datetime import date
+import datetime
 
-def main(d):
-	output_filename = d + os.sep + "result.csv"
-	db = sqlite3.connect(':memory:')
-	#db = sqlite3.connect(d + os.sep + "debug.db")
-	df = None
-	for filename in glob.glob(d + os.sep + "*.csv"):
-		if filename == output_filename:
-			continue
-		print("loading", filename)
-		tmp = pd.read_csv(filename, dtype=str)
-		tmp['date'] = pd.to_datetime(tmp['公表_年月日'])
-		tmp = tmp.loc[:,['date']]
-		if df == None:
-			df = tmp
-		else:
-			df = pd.concat([df, tmp], axis=0)
-	df = df.assign(counter=1)
-	dt = pd.date_range('2020-01-01', date.today(),  freq='D')
-	dt = dt.to_frame(index=False, name="date").assign(counter=0)
-	df = pd.concat([df, dt], axis=0)
-	df.to_sql('covid19', db, if_exists='append', index=None)
-	df = pd.read_sql_query('SELECT date, SUM(counter) AS counter FROM covid19 group by date;', db)
-	df.to_csv(output_filename)
-	db.close()
+def main(d, outputcsvfile, outputimgfile):
+    srclabel = '公表_年月日'
+    df = pd.DataFrame()
+    for filename in glob.glob(d + os.sep + "*.csv"):
+        if filename != d + os.sep + outputcsvfile:
+            tmp = pd.read_csv(filename, dtype=str).fillna(0)
+            if df.empty:
+                df = tmp
+            else:
+                df = pd.concat([df, tmp])
+    s = pd.to_datetime(df[srclabel]).value_counts()
+    df = pd.DataFrame(s)
+    df = df.rename(columns={srclabel: 'positive'})
+    df['date'] = df.index
+    mindate = min(df.index)-datetime.timedelta(days=7)
+    maxdate = max(df.index)
+    dates = pd.date_range(start=mindate, end=maxdate, freq='D')
+    df2 = pd.DataFrame({'date': dates})
+    df = pd.merge(df2, df, how='left').fillna(0)
+    df['7days_mean'] = df['positive'].rolling(7).mean().fillna(0)
+    df.to_csv(d + os.sep + outputcsvfile)
+    #
+    fig = plt.figure(dpi=100, figsize=(16, 9))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(df['date'], df['positive'], color='blue')
+    ax.plot(df['date'], df['7days_mean'], color='red')
+    plt.savefig(d + os.sep + outputimgfile)
 
 if __name__ == '__main__':
-	main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
